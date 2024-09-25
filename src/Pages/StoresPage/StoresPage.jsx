12cc4ceb7552aef532bc953d80182dec6b45c3b4
md rasif <mdrasif3499@gmail.com>
2024-09-26 00:01:10 +0600

    add some new

--- src/Pages/StoresPage/StoresPage.jsx
+++ src/Pages/StoresPage/StoresPage.jsx
@@ -36,7 +36,7 @@ const StoresPage = () => {
         </div>
       </div>
       <div className="space-y-2">
-        <h1 className="text-[18px] font-semibold">
+        <h1 className="text-[18px] font-bold">
           Online Store of Household Appliances and Electronics
         </h1>
         <p>
@@ -144,6 +144,15 @@ const StoresPage = () => {
             </div>
           </div>
         </div>
+        <h6 className="text-[15px] font-bold">A wonderful serenity has taken possession of my entire soul</h6>
+        <p>I should be incapable of drawing a single stroke at the present moment; and yet I feel that I never was a greater artist than now.</p>
+        <h6 className="text-[15px] font-bold">When, while the lovely valley teems with vapour around me
+        </h6>
+        <p>little world among the stalks, and grow familiar with the countless indescribable forms of the insects and flies, then I feel the presence of the Almighty, who formed us in his own image, and the breath of that universal love which bears and sustains us, as it floats around us in an eternity of bliss; and then, my friend, when darkness overspreads my eyes, and heaven and earth seem to dwell in my soul and absorb its power, like the form of a beloved mistress, then I often think with longing, Oh, would I could describe these conceptions, could impress upon paper all that is living so full and warm within me, that it might be the mirror of my soul, as my soul is the mirror of the infinite God!</p>
+        <h6 className="text-[15px] font-bold">Online shopping that really is convenient
+        </h6>
+        <p>The car parts and everything you may need for repairs and regular maintenance of your vehicle are listed in a convenient and comprehensive catalogue. The innovative search – by name, item ID or OEM number will help you to find automotive parts easily.</p>
+        <p>You can choose whichever payment method is most convenient for you from among the various options. Have any questions? Our support service specialists are always on hand to help. Picking and buying car parts with us is an enjoyable experience!</p>
       </div>
     </div>
   );
