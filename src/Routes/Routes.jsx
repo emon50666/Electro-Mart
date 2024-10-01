cd52dadd9083402da5c03f78bdb9a8d0ef48d6a2
md rasif <mdrasif3499@gmail.com>
2024-10-01 19:30:32 +0600

    storPage complete

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -9,6 +9,7 @@ import Login from "../components/Login/Login";
 import Register from "../components/Login/Register/Register";
 import ProductDetails from "../Pages/ProductDetails/ProductDetails";
 import ComparePage from "../Pages/ComparePage/ComparePage";
+import StorPage from "../Pages/StorPage/StorPage";
 const router = createBrowserRouter([
   {
     path: "/",
@@ -26,6 +27,11 @@ const router = createBrowserRouter([
       {
         path: "/comparePage",
         element: <ComparePage />
+      }
+      ,
+      {
+        path: "/storPage",
+        element:<StorPage></StorPage>
       }
     ],
 
