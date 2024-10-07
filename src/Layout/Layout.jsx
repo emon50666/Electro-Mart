09dc6f5e4cfd1d5f1d54d1db3c0181e3465fc79c
Ridwan4D <ridwanul161@gmail.com>
2024-10-08 00:41:20 +0600

    footer added with responsiveness

--- src/Layout/Layout.jsx
+++ src/Layout/Layout.jsx
@@ -1,18 +1,18 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../components/Navbar/Navbar";
-
 import MenuBar from "../components/Navbar/MenuBar";
 import BottomNavigation from "./BottomNavigation";
+import Footer from "../components/Footer/Footer";
 
 const Layout = () => {
     return (
         <div className="mx-w[2550px] mx-auto">
             <Navbar />
-            <MenuBar ></MenuBar>
-
+            <MenuBar />
             <Outlet />
-             <BottomNavigation/>
-            
+            <BottomNavigation />
+            <Footer />
+
         </div>
     );
 };
