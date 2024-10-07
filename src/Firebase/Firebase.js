05c1b90bf714ba47d0811652d17cda95e315536f
Emon <emon50666@gmail.com>
2024-10-07 21:44:37 +0600

    error solve

--- src/Firebase/Firebase.js
+++ src/Firebase/Firebase.js
@@ -1,17 +1,42 @@
+// // Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// import { getAuth } from "firebase/auth";
+// const firebaseConfig = {
+//   apiKey: import.meta.env.VITE_APIKEY,
+//   authDomain: import.meta.env.VITE_AUTHDOMAIN,
+//   projectId: import.meta.env.VITE_PROJECTID,
+//   storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+//   messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+//   appId: import.meta.env.VITE_APPID,
+//   measurementId: import.meta.env.VITE_MEASUREMENTID
+// };
+
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+// const auth = getAuth(app);
+// export default auth;
+
+
+
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_APIKEY,
-  authDomain: import.meta.env.VITE_AUTHDOMAIN,
-  projectId: import.meta.env.VITE_PROJECTID,
-  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
-  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
-  appId: import.meta.env.VITE_APPID,
-  measurementId: import.meta.env.VITE_MEASUREMENTID
+  apiKey: "AIzaSyBbYHMsSWFkrgK91iNVGUQqErKRUgm2F0g",
+  authDomain: "electro-mart-client.firebaseapp.com",
+  projectId: "electro-mart-client",
+  storageBucket: "electro-mart-client.appspot.com",
+  messagingSenderId: "488862218023",
+  appId: "1:488862218023:web:ed6467c66f9ac625c55b02",
+  measurementId: "G-JWLX3SPN9R"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
-export default auth;
+ const auth = getAuth(app);
+ export default auth;
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -98,32 +98,23 @@ const Navbar = () => {
             )}
           </div>
         </div>
-          {user && (
-            <ul tabIndex={0} className={`menu menu-sm  font-semibold right-0 dropdown-content bg-white overflow-hidden rounded-box w-40 ${isOpen ? "translate-x-0" : "translate-y-0"}  border border-dashed border-orange-300 `}>
-              <Link to={'/dashboard'} className="hover:bg-orange-50 p-2 rounded-md hover:text-orange-500"><li>DashBoard</li></Link>
-              <Link className="hover:bg-orange-50 p-2 rounded-md hover:text-orange-500"><li>Profile</li></Link>
-              <Link className="hover:bg-orange-50 p-2 rounded-md hover:text-orange-500"><li>Setting</li></Link>
-              <Link to={'/register'} className="hover:bg-orange-50 p-2 rounded-md hover:text-orange-500">
-                <button onClick={logOut}><a>Logout</a></button>
-              </Link>
-            </ul>
-          )}
+          
         </div>
         <div className="hidden lg:flex space-x-3 items-center mt-2">
           {/* Cart Button */}
-          <button onClick={handleAddToCart} className="relative bg-orange-200/50 hover:bg-orange-300/50 cursor-pointer rounded-full p-2 w-10 h-10 flex items-center justify-center">
+          <button onClick={handleAddToCart} className="relative bg-orange-200/50 hover:bg-orange-300/50 cursor-pointer  rounded-full p-2">
             <FaCartShopping className="text-lg text-orange-600" />
             <span className="absolute -right-1 -top-1 rounded-full bg-orange-500 px-1 py-[1px] text-[10px] text-white">9+</span>
           </button>
 
           {/* Compare Link */}
-          <Link to="/comparePage" className="relative bg-orange-200/50 hover:bg-orange-300/50 cursor-pointer rounded-full p-2 w-10 h-10 flex items-center justify-center">
+          <Link to="/comparePage" className="relative bg-orange-200/50 hover:bg-orange-300/50 cursor-pointer  rounded-full p-2">
             <IoGitCompareOutline className="text-lg text-orange-600" />
             <span className="absolute -right-1 -top-1 rounded-full bg-orange-500 px-1 py-[1px] text-[10px] text-white">9+</span>
           </Link>
 
 {/* Wishlist Button */}
-<div className="relative bg-orange-200/50 hover:bg-orange-300/50 cursor-pointer rounded-full p-2 w-10 h-10 flex items-center justify-center">
+<div className="relative bg-orange-200/50 hover:bg-orange-300/50 cursor-pointer  rounded-full p-2">
   <FaHeart className="text-lg text-orange-600" />
   <span className="absolute -right-1 -top-1 rounded-full bg-orange-500 px-1 py-[1px] text-[10px] text-white">9+</span>
 </div>
@@ -131,21 +122,16 @@ const Navbar = () => {
         <div className="dropdown dropdown-end">
           <div tabIndex={0} role="button" className="p-1 rounded-full">
             {user ? (
-              <div className="relative inline-block">
-                <img title={user?.displayName} src={user?.photoURL} className="w-10 h-10 rounded-full border-2 border-blue-600 p-0.5" />
-                <span className="h-3 w-3 rounded-full border border-white bg-green-500 block absolute top-1 right-0"></span>
-              </div>
+             <div className="relative inline-block">
+             <img title={user?.displayName} src={user?.photoURL} className="w-10 h-10 rounded-full border-2 border-blue-600 " />
+             <span className="h-3 w-3 rounded-full border border-white bg-green-500 block absolute top-1 right-0"></span>
+           </div> 
             ) : loading ? (
               <div className="relative inline-block">
                 <img src={user?.photoURL} className="w-10 h-10 rounded-full border-2 border-blue-600 p-0.5" />
                 <span className="h-3 w-3 rounded-full border border-white bg-green-500 block absolute top-1 right-0"></span>
               </div>
-            ) : loading ? (
-                <div className="relative inline-block">
-                  <img src={user?.photoURL} className="w-10 h-10 rounded-full border-2 border-blue-600 p-0.5" />
-                  <span className="h-3 w-3 rounded-full border border-white bg-green-500 block absolute top-1 right-0"></span>
-                </div>
-                ) : (
+            )  : (
                 <Link to={'/register'}>
                   <div className="relative bg-orange-200/50 hover:bg-orange-300/50 cursor-pointer rounded-full p-2">
                     <MdAccountCircle className="text-lg text-orange-600" />
