a0cf1d374d265066613f559ce5b948673c31cf95
Ridwan4D <ridwanul161@gmail.com>
2024-10-12 13:37:34 +0600

    new footer is responsive nao

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -5,14 +5,14 @@ import { Link } from "react-router-dom"; // Import Link from react-router-dom
 const Footer = () => {
     return (
         <div className="px-4 pt-16 md:px-24 lg:px-8 bg-[#222222] text-white font_inter">
-            <div className="grid gap-16 row-gap-10 mb-8 lg:grid-cols-6">
-                <div className="md:max-w-md lg:col-span-2">
-                    <h2 className="font-bold text-5xl">Elector <span className="text-orange-500">Mart</span></h2>
+            <div className="grid gap-16 row-gap-10 mb-8 grid-cols-1 md:grid-cols-3 xl:grid-cols-6">
+                <div className="md:max-w-md col-span-1 md:col-span-2">
+                    <h2 className="font-bold text-3xl xl:text-5xl">Elector <span className="text-orange-500">Mart</span></h2>
                     <div className="mt-6 lg:max-w-sm">
                         <p className="text-lg text-[#B3B3B3]">
                             Call us 24/7
                         </p>
-                        <p className="font-semibold text-3xl text-white">
+                        <p className="font-semibold text-2xl xl:text-3xl text-white">
                             +880 15 xxxx xxxx
                         </p>
                     </div>
@@ -49,136 +49,136 @@ const Footer = () => {
                         </ul>
                     </div>
                 </div>
-                <div className="grid grid-cols-2 gap-5 row-gap-8 lg:col-span-4 md:grid-cols-4">
-                    <div>
-                        <p className="font-semibold text-xl tracking-wide text-[#818181]">
-                            Our Story
-                        </p>
-                        <ul className="mt-2 space-y-2 text-lg">
-                            <li>
-                                <Link to="/" className="text-white underline-hover">
-                                    Web
-                                </Link>
-                            </li>
-                            <li>
-                                <Link to="/" className="text-white underline-hover">
-                                    eCommerce
-                                </Link>
-                            </li>
-                            <li>
-                                <Link to="/" className="text-white underline-hover">
-                                    Business
-                                </Link>
-                            </li>
-                            <li>
-                                <Link to="/" className="text-white underline-hover">
-                                    Entertainment
-                                </Link>
-                            </li>
-                            <li>
-                                <Link to="/" className="text-white underline-hover">
-                                    Portfolio
-                                </Link>
-                            </li>
-                        </ul>
-                    </div>
-                    <div>
-                        <p className="font-semibold text-xl tracking-wide text-[#818181]">
-                            Category
-                        </p>
-                        <ul className="mt-2 space-y-2 text-lg">
-                            <li>
-                                <Link to="/" className="text-white underline-hover">
-                                    News
-                                </Link>
-                            </li>
-                            <li>
-                                <Link to="/" className="text-white underline-hover">
-                                    World
-                                </Link>
-                            </li>
-                            <li>
-                                <Link to="/" className="text-white underline-hover">
-                                    Games
-                                </Link>
-                            </li>
-                            <li>
-                                <Link to="/" className="text-white underline-hover">
-                                    References
-                                </Link>
-                            </li>
-                        </ul>
-                    </div>
-                    <div>
-                        <p className="font-semibold text-xl tracking-wide text-[#818181]">
-                            Quick Links
-                        </p>
-                        <ul className="mt-2 space-y-2 text-lg">
-                            <li>
-                                <Link to="/" className="text-white underline-hover">
-                                    Media
-                                </Link>
-                            </li>
-                            <li>
-                                <Link to="/" className="text-white underline-hover">
-                                    Brochure
-                                </Link>
-                            </li>
-                            <li>
-                                <Link to="/" className="text-white underline-hover">
-                                    Nonprofit
-                                </Link>
-                            </li>
-                            <li>
-                                <Link to="/" className="text-white underline-hover">
-                                    Educational
-                                </Link>
-                            </li>
-                            <li>
-                                <Link to="/" className="text-white underline-hover">
-                                    Projects
-                                </Link>
-                            </li>
-                        </ul>
-                    </div>
-                    <div>
-                        <p className="font-semibold text-xl tracking-wide text-[#818181]">
-                            Contact Us
-                        </p>
-                        <ul className="mt-2 space-y-2 text-lg">
-                            <li>
-                                <Link to="/" className="text-white underline-hover">
-                                    Infopreneur
-                                </Link>
-                            </li>
-                            <li>
-                                <Link to="/" className="text-white underline-hover">
-                                    Personal
-                                </Link>
-                            </li>
-                            <li>
-                                <Link to="/" className="text-white underline-hover">
-                                    Wiki
-                                </Link>
-                            </li>
-                            <li>
-                                <Link to="/" className="text-white underline-hover">
-                                    Forum
-                                </Link>
-                            </li>
-                        </ul>
-                    </div>
+                <div className="col-span-1">
+                    <p className="font-semibold text-xl tracking-wide text-[#818181]">
+                        Our Story
+                    </p>
+                    <ul className="mt-2 space-y-1 md:space-y-2 text-lg">
+                        <li>
+                            <Link to="/" className="text-white underline-hover">
+                                Web
+                            </Link>
+                        </li>
+                        <li>
+                            <Link to="/" className="text-white underline-hover">
+                                eCommerce
+                            </Link>
+                        </li>
+                        <li>
+                            <Link to="/" className="text-white underline-hover">
+                                Business
+                            </Link>
+                        </li>
+                        <li>
+                            <Link to="/" className="text-white underline-hover">
+                                Entertainment
+                            </Link>
+                        </li>
+                        <li>
+                            <Link to="/" className="text-white underline-hover">
+                                Portfolio
+                            </Link>
+                        </li>
+                    </ul>
+                </div>
+                <div className="col-span-1">
+                    <p className="font-semibold text-xl tracking-wide text-[#818181]">
+                        Category
+                    </p>
+                    <ul className="mt-2 space-y-1 md:space-y-2 text-lg">
+                        <li>
+                            <Link to="/" className="text-white underline-hover">
+                                News
+                            </Link>
+                        </li>
+                        <li>
+                            <Link to="/" className="text-white underline-hover">
+                                World
+                            </Link>
+                        </li>
+                        <li>
+                            <Link to="/" className="text-white underline-hover">
+                                Games
+                            </Link>
+                        </li>
+                        <li>
+                            <Link to="/" className="text-white underline-hover">
+                                References
+                            </Link>
+                        </li>
+                    </ul>
+                </div>
+                <div className="col-span-1">
+                    <p className="font-semibold text-xl tracking-wide text-[#818181]">
+                        Quick Links
+                    </p>
+                    <ul className="mt-2 space-y-1 md:space-y-2 text-lg">
+                        <li>
+                            <Link to="/" className="text-white underline-hover">
+                                Media
+                            </Link>
+                        </li>
+                        <li>
+                            <Link to="/" className="text-white underline-hover">
+                                Brochure
+                            </Link>
+                        </li>
+                        <li>
+                            <Link to="/" className="text-white underline-hover">
+                                Nonprofit
+                            </Link>
+                        </li>
+                        <li>
+                            <Link to="/" className="text-white underline-hover">
+                                Educational
+                            </Link>
+                        </li>
+                        <li>
+                            <Link to="/" className="text-white underline-hover">
+                                Projects
+                            </Link>
+                        </li>
+                    </ul>
+                </div>
+                <div className="col-span-1">
+                    <p className="font-semibold text-xl tracking-wide text-[#818181]">
+                        Contact Us
+                    </p>
+                    <ul className="mt-2 space-y-1 md:space-y-2 text-lg">
+                        <li>
+                            <Link to="/" className="text-white underline-hover">
+                                Infopreneur
+                            </Link>
+                        </li>
+                        <li>
+                            <Link to="/" className="text-white underline-hover">
+                                Personal
+                            </Link>
+                        </li>
+                        <li>
+                            <Link to="/" className="text-white underline-hover">
+                                Wiki
+                            </Link>
+                        </li>
+                        <li>
+                            <Link to="/" className="text-white underline-hover">
+                                Forum
+                            </Link>
+                        </li>
+                    </ul>
                 </div>
             </div>
             <div className="flex flex-col justify-between pt-5 pb-10 border-t sm:flex-row">
                 <p className="text-lg">
                     Copyright © 2024 Electro Mart. Created by Elector Team
                 </p>
-                <div className="flex items-center mt-4 space-x-4 sm:mt-0">
+                <div className="flex flex-col md:flow-row md:items-center mt-4 space-x-4 sm:mt-0 mb-7 md:mb-auto">
                     <p className="text-lg">
                         We using safe payment for
                     </p>
-
+                    <div>
+                        <img src="https://res.cloudinary.com/duv5fiurz/image/upload/v1728717572/Payment_2_yu9afc.svg" alt="" />
+                    </div>
                 </div>
             </div>
         </div>
