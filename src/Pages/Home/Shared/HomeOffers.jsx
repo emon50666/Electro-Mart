d5cf7ddbd61561cc180960e9ebb7a1f731398c09
Ridwan4D <ridwanul161@gmail.com>
2024-10-19 23:50:22 +0600

    if any product will stock out it will remove from home best offer

--- src/Pages/Home/Shared/HomeOffers.jsx
+++ src/Pages/Home/Shared/HomeOffers.jsx
@@ -3,7 +3,7 @@ import ProductCard from "../../../components/ProductCard/ProductCard";
 import useProduct from "../../../Hooks/useProduct";
 
 const HomeOffers = () => {
-  const { products, refetch ,isLoading} = useProduct();
+  const { products, refetch, isLoading } = useProduct();
   if (isLoading) return <Loader />;
   return (
     <div className="px-8 font_open_sense">
@@ -14,7 +14,7 @@ const HomeOffers = () => {
       <div className="flex lg:grid lg:grid-cols-5 gap-3 md:grid-cols-3 sm:grid-cols-1 overflow-x-auto lg:overflow-hidden no-scrollbar snap-x snap-mandatory">
         {products.slice(0, 5).map((product, idx) => (
           <>
-            {(product?.isHot === "yes" || product?.isNew === "yes" || product?.discountPercentage > 1) && (
+            {((product?.isHot === "yes" || product?.isNew === "yes" || product?.discountPercentage > 1) && product?.quantity > 0) && (
               <div key={idx} className="snap-start flex-shrink-0 w-full sm:w-auto">
                 <ProductCard product={product} refetch={refetch} />
               </div>
