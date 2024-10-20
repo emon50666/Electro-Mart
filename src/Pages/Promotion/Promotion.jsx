56af067612b73693c49bc0b9917777d167a77d6c
md rasif <mdrasif3499@gmail.com>
2024-10-20 12:33:11 +0600

    add animation image

--- src/Pages/Promotion/Promotion.jsx
+++ src/Pages/Promotion/Promotion.jsx
@@ -10,34 +10,32 @@ const Promotion = () => {
       <h2 className="text-2xl">Promotions</h2>
       <div className="grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-3">
         {promotionsProducts.map((product) => (
+          <div
+            key={product._id}
+            className="relative rounded-lg overflow-hidden bg-gray-100 shadow-lg group"
+          >
+            <img
+              src={product.image}
+              className="lg:h-[280px] lg:w-[480px]  object-cover transition-transform duration-500 ease-in-out transform group-hover:scale-110"
+              alt={product.title}
+            />
 
+            <div className="absolute top-5 left-4 text-sm text-slate-300">
+              <p>
+                {product.offerStartDate} - {product.offerRemoveDate}
+              </p>
+            </div>
 
-         <div
-  key={product._id}
-  className="relative rounded-lg overflow-hidden bg-gray-100 shadow-lg group"
->
-  <img
-    src={product.image}
-    className="lg:h-[280px] lg:w-[480px] object-cover transition-transform duration-500 ease-in-out transform group-hover:scale-110"
-    alt={product.title}
-  />
-  <div className="absolute top-5 left-4 text-sm text-slate-300">
-    <p>
-      {product.offerStartDate} - {product.offerRemoveDate}
-    </p>
-  </div>
-  
-  <div className="absolute bottom-4 left-4 flex flex-col space-y-2">
-    <h1 className="lg:text-2xl text-xl font-semibold text-white">
-      {product.title}
-    </h1>
-    <button className="bg-orange-400 hover:bg-orange-500 text-xs lg:text-[12px] text-white py-2 lg:px-4 px-3 w-32">
-      Read More
-    </button>
-  </div>
-</div>
+            <div className="absolute bottom-4 left-4 flex flex-col space-y-2">
+              <h1 className="lg:text-2xl text-xl font-semibold text-white transform transition-transform duration-500 ease-in-out group-hover:translate-y-0 translate-y-10">
+                {product.title}
+              </h1>
 
-
+              <button className="bg-orange-400 hover:bg-orange-500 text-xs lg:text-[12px] text-white py-2 lg:px-4 px-3 w-32 opacity-0 transition-opacity duration-700 ease-in-out group-hover:opacity-100">
+                Read More
+              </button>
+            </div>
+          </div>
         ))}
       </div>
     </div>
