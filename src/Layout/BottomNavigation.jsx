568a09f03d654557bb5a1e3cc33d920bbd0bc438
Ridwan4D <ridwanul161@gmail.com>
2024-10-21 15:11:23 +0600

    manage cart is linked up with bottom navigation cart

--- src/Layout/BottomNavigation.jsx
+++ src/Layout/BottomNavigation.jsx
@@ -1,6 +1,7 @@
 
 import { FaHome, FaThList, FaShoppingCart, FaSearch } from 'react-icons/fa'; // Importing icons from react-icons
 import useCart from '../Hooks/useCart';
+import { Link } from 'react-router-dom';
 
 const BottomNavigation = () => {
   const { theUserCarts } = useCart();
@@ -18,25 +19,25 @@ const BottomNavigation = () => {
           <FaThList className="text-2xl" />
           <span className="text-xs">Category</span>
         </div>
-         {/* Category Icon */}
-         <div className="flex flex-col items-center text-gray-600">
+        {/* Category Icon */}
+        <div className="flex flex-col items-center text-gray-600">
           <FaSearch className="text-2xl" />
           <span className="text-xs">Search</span>
         </div>
 
         {/* Cart Icon */}
-        <div className="relative flex flex-col items-center text-gray-600">
+        <Link to={'/manageCart'} className="relative flex flex-col items-center text-gray-600">
           <FaShoppingCart className="text-2xl" />
           <span className="text-xs">Cart</span>
           {/* Badge for cart items count */}
           {
-              theUserCarts.length > 0 && (
-                <span className="absolute -right-2 -ml-1 -top-2 rounded-full bg-orange-500 px-1 py-[1px] text-[10px] text-white">
-                  {theUserCarts.length + '+'}
-                </span>
-              )
-            }
-        </div>
+            theUserCarts.length > 0 && (
+              <span className="absolute -right-2 -ml-1 -top-2 rounded-full bg-orange-500 px-1 py-[1px] text-[10px] text-white">
+                {theUserCarts.length + '+'}
+              </span>
+            )
+          }
+        </Link>
       </nav>
     </div>
   );
