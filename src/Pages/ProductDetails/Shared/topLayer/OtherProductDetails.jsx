abaf0d375e82d504965b18adb35430af217b1208
Emon <emon50666@gmail.com>
2024-10-22 14:08:03 +0600

    change product details page

--- src/Pages/ProductDetails/Shared/topLayer/OtherProductDetails.jsx
+++ src/Pages/ProductDetails/Shared/topLayer/OtherProductDetails.jsx
@@ -18,7 +18,7 @@ import useAddToCompare from "../../../../Hooks/useAddToCompare";
 import useAddToWishlist from "../../../../Hooks/useAddToWishlist";
 import useAxiosPublic from "../../../../Hooks/useAxiosPublic";
 import useProduct from "../../../../Hooks/useProduct";
-import ReactHtmlParser from 'react-html-parser';
+
 const OtherProductDetails = ({ product }) => {
     const axiosPublic = useAxiosPublic();
     const { refetch } = useProduct();
@@ -101,8 +101,7 @@ const OtherProductDetails = ({ product }) => {
                 </div>
                 <div className="font_cabin text-sm lg:text-base text-[#777777]">
                     <p>
-                        {/* {product?.shortDescription} */}
-                        {ReactHtmlParser(product?.shortDescription)}
+                        {product?.shortDescription}
                     </p>
                 </div>
                 {/* Price & description end */}
