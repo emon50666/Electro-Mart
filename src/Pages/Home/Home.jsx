1f56bba8b44b3c287299d260c187886fb0307f86
Ridwan4D <ridwanul161@gmail.com>
2024-10-25 21:25:07 +0600

    fau changes heppen

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,27 +1,16 @@
 import { Helmet } from "react-helmet";
-
 import HomeOffers from "./Shared/HomeOffers";
 import Popular from "../../components/PopularCategory/Popular";
 import BenifetService from "../../components/PopularCategory/BenifetService/BenifetService";
 import HomeSliderComponent from "../../components/HomeSliderComponent/SliderComponent";
-
 // import FilterProduct from "../../components/FilterProduct/FilterProduct";
-
 import Slider from "./Shared/Slider"
-
-// import Banner from "./Shared/Banner";
-
-
 const Home = () => {
   return (
     <div className="bg-[#F6F6F6]">
       <Helmet>
         <title>Home | Elector Mart</title>
       </Helmet>
-
-      {/* <div className=" flex justify-center min-h-[80vh]">
-        <Banner />
-      </div> */}
       <div>
         <Slider />
       </div>
@@ -30,12 +19,7 @@ const Home = () => {
       <HomeOffers />
       {/* <FilterProduct /> */}
       <HomeSliderComponent />
-
-
-
-
     </div>
   );
 };
-
 export default Home;
