f6d656812c03936399d8b6425a2e37cf3bd5dd3a
Ridwan4D <ridwanul161@gmail.com>
2024-10-25 19:55:21 +0600

    component created for get image of right side top

--- src/Pages/Home/Shared/RightSide/RightSideTop.jsx
+++ src/Pages/Home/Shared/RightSide/RightSideTop.jsx
@@ -1,46 +1,38 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import { MdModeEdit } from "react-icons/md";
 import { Link } from 'react-router-dom';
 import useRoll from '../../../../Hooks/useRoll';
 import RightSideModal from './RightSideModal';
-import useAxiosPublic from "../../../../Hooks/useAxiosPublic";
+import useRightSideTop from "../../../../Hooks/useRightSideTop";
 
 const RightSideTop = () => {
     const [role] = useRoll();
-    const axiosPublic = useAxiosPublic();
+    const { rightTopImages, refetch } = useRightSideTop();
     const [isModalOpen, setIsModalOpen] = useState(false);
-    const [rightTopImages, setRightTopImages] = useState(null);
 
     const openModal = () => setIsModalOpen(true);
     const closeModal = () => setIsModalOpen(false);
 
-    useEffect(() => {
-        const fetchRightTopImages = async () => {
-            try {
-                const res = await axiosPublic.get("/rightTop");
-                setRightTopImages(res.data);
-            } catch (error) {
-                console.error("Error fetching rightTopImages:", error);
-            }
-        };
 
-        fetchRightTopImages();
-    }, [axiosPublic]);
 
     return (
         <div>
             <div className="relative">
-                <img
-                    src={rightTopImages?.url || "https://res.cloudinary.com/duv5fiurz/image/upload/v1729834654/41f528d4-756d-4f7b-a1c6-3da74cfe26fb_midfym.jpg"}
-                    alt="slide 1"
-                    className="w-full lg:min-h-[150px] max-h-[190px] h-[40vh] md:h-auto rounded-lg"
-                />
+                {rightTopImages.map((rightBottomLImage, idx) => (
+                    <div key={idx}>
+                        <img
+                            src={rightTopImages[0]?.url}
+                            alt={`slide ${idx + 1}`}
+                            className="w-full lg:min-h-[150px] max-h-[190px] h-[40vh] md:h-auto rounded-lg"
+                        />
 
-                <div className="absolute inset-0 flex flex-col items-start justify-center space-y-4 md:left-5 lg:left-8 px-1">
-                    <Link to="/" className="bg-blue-500 text-white py-2 px-4 rounded-lg shadow-lg hover:bg-blue-600 transition">
-                        Shop Now
-                    </Link>
-                </div>
+                        <div className="absolute inset-0 flex flex-col items-start justify-center space-y-4 md:left-5 lg:left-8 px-1">
+                            <Link to="/" className="bg-blue-500 text-white py-2 px-4 rounded-lg shadow-lg hover:bg-blue-600 transition">
+                                Shop Now
+                            </Link>
+                        </div>
+                    </div>
+                ))}
 
                 {role === "admin" && (
                     <span
@@ -53,7 +45,7 @@ const RightSideTop = () => {
             </div>
 
             {/* Modal */}
-            <RightSideModal isOpen={isModalOpen} onClose={closeModal} number={1} />
+            <RightSideModal isOpen={isModalOpen} onClose={closeModal} sendImages={rightTopImages} refetch={refetch} number={1} />
         </div>
     );
 };
