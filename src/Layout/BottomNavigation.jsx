4c13ac67d3b2014c9598661248a90ce331dcc4c0
Emon <emon50666@gmail.com>
2024-10-31 15:48:53 +0600

    admin dashboard design change

--- src/Layout/BottomNavigation.jsx
+++ src/Layout/BottomNavigation.jsx
@@ -2,9 +2,11 @@
 import { FaHome, FaThList, FaShoppingCart, FaSearch } from 'react-icons/fa'; // Importing icons from react-icons
 import useCart from '../Hooks/useCart';
 import { Link } from 'react-router-dom';
+import UserAuth from '../Hooks/useAuth';
 
 const BottomNavigation = () => {
   const { theUserCarts } = useCart();
+  const {  loading,  } = UserAuth();
   return (
     <div className="lg:hidden fixed bottom-0 left-0 right-0 bg-white z-30 shadow-lg border-t border-gray-200">
       <nav className="flex justify-between items-center h-16 px-4">
@@ -30,13 +32,15 @@ const BottomNavigation = () => {
           <FaShoppingCart className="text-2xl" />
           <span className="text-xs">Cart</span>
           {/* Badge for cart items count */}
-          {
-            theUserCarts.length > 0 && (
-              <span className="absolute -right-2 -ml-1 -top-2 rounded-full bg-blue-500 px-1 py-[1px] text-[10px] text-white">
-                {theUserCarts.length + '+'}
-              </span>
-            )
-          }
+          {!loading && (
+              theUserCarts.length > 0 && (
+                <span className="absolute -right-1 -ml-1 -top-2 h-4 w-4 rounded-full bg-blue-500 px-1 py-0 text-[10px] text-white flex items-center justify-center">
+                 
+
+                  {theUserCarts.length >= 9 ? "9+" : theUserCarts.length}
+          </span>
+          )
+            )}
         </Link>
       </nav>
     </div>
