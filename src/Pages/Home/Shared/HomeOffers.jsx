29b8741d46a96dc084fcf8ba61c4ded17782db3a
Ridwanul Islam <ridwanul161@gmail.com>
2024-11-15 21:43:05 +0300

    update:best product of home page showing discountPercentage availabled product

--- src/Pages/Home/Shared/HomeOffers.jsx
+++ src/Pages/Home/Shared/HomeOffers.jsx
@@ -5,25 +5,26 @@ import useProduct from "../../../Hooks/useProduct";
 const HomeOffers = () => {
   const { products, refetch, isLoading } = useProduct();
   if (isLoading) return <Loader />;
+
+  // Filter products based on discountPercentage
+  const filteredProducts = products.filter(
+    (product) =>
+      product?.discountPercentage && product?.isHot != "yes" && product?.isNew != "yes" && product?.quantity > 0
+  );
+
   return (
-    <div className="px-2 lg:px-8  font_open_sense">
+    <div className="px-2 lg:px-8 font_open_sense">
       <div>
         <h3 className="text-2xl mb-5 font-bold">The Best Offers</h3>
       </div>
-      {/* preview cards */}
+      {/* Preview cards */}
       <div className="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-5 gap-3">
-        {products.slice(0, 5).map((product, idx) => (
-          <>
-            {((product?.isHot === "yes" ) && product?.quantity > 0) && (
-              <div key={idx} className="snap-start flex-shrink-0 w-full sm:w-auto">
-                <ProductCard product={product} refetch={refetch} />
-              </div>
-            )}
-          </>
+        {filteredProducts.slice(0, 5).map((product, idx) => (
+          <div key={idx} className="snap-start flex-shrink-0 w-full sm:w-auto">
+            <ProductCard product={product} refetch={refetch} />
+          </div>
         ))}
       </div>
-
-
     </div>
   );
 };
