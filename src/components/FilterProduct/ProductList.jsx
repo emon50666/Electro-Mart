1352f44f0093fcb1715679fc7f6ccddc8e5f00e1
Ridwanul Islam <ridwanul161@gmail.com>
2024-12-05 22:32:37 +0300

    shop page is update

--- src/components/FilterProduct/ProductList.jsx
+++ src/components/FilterProduct/ProductList.jsx
@@ -2,17 +2,19 @@
 import ProductCard from "../ProductCard/ProductCard";
 
 const ProductList = ({ filteredProducts }) => {
-    // // console.log(filteredProducts);
-    return (
-        <div className=" grid lg:grid-cols-4 gap-5 bg-[#F6F6F6] md:grid-cols-3 sm:grid-cols-1 overflow-x-auto lg:overflow-hidden no-scrollbar snap-x snap-mandatory">
-            {filteredProducts.map(product => (
-              
-                <div key={product._id} className="snap-start  flex-shrink-0 w-full sm:w-auto">
-                    <ProductCard product={product} />
-                </div>
-            ))}
+  // // console.log(filteredProducts);
+  return (
+    <div className=" grid lg:grid-cols-4 xl:grid-cols-5 gap-6 bg-[#F6F6F6] md:grid-cols-3 sm:grid-cols-1 overflow-x-auto lg:overflow-hidden no-scrollbar">
+      {filteredProducts.map((product) => (
+        <div
+          key={product._id}
+          className="snap-start  flex-shrink-0 w-full sm:w-auto"
+        >
+          <ProductCard product={product} />
         </div>
-    );
+      ))}
+    </div>
+  );
 };
 
 export default ProductList;
