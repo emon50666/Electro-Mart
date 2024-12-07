f5012bd7ce1338b59ad3ed75d66814ad5b850fcd
Ridwanul Islam <ridwanul161@gmail.com>
2024-12-07 16:59:19 +0300

    in user dashboard showing the top sale product

--- src/components/Navbar/MenuBar.jsx
+++ src/components/Navbar/MenuBar.jsx
@@ -63,8 +63,8 @@ const MenuBar = () => {
           </button>
           {/* Dropdown Menu */}
           {isOpen && (
-            <div className="absolute top-full left-5 z-40 w-full bg-white shadow-lg border border-gray-200 mt-2 rounded-lg overflow-hidden px-5 py-3">
-              <ul className="grid lg:grid-cols-10 xl:grid-cols-12 gap-y-2">
+            <div className="absolute top-full left-5 z-40 w-full bg-white shadow-lg border border-gray-200 mt-2 rounded-lg overflow-hidden px-5 py-3 max-w-[1700px]">
+              <ul className="grid lg:grid-cols-7 xl:grid-cols-10 gap-y-2">
                 {categories.map(
                   (cat, idx) =>
                     cat.newCategory && (
