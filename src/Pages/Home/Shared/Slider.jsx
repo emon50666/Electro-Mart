209d13ff9cf6ea52c8157e5ee293e550d20fff19
Ridwanul Islam <ridwanul161@gmail.com>
2024-12-13 23:47:02 +0300

    slider responsive for sm device

--- src/Pages/Home/Shared/Slider.jsx
+++ src/Pages/Home/Shared/Slider.jsx
@@ -3,7 +3,7 @@ import RightSlide from "./RightSlide";
 
 const Slider = () => {
     return (
-        <div className="mx-auto  flex flex-col lg:flex-row items-stretch justify-center  pt-5 md:pt-6 lg:pt-2 gap-3 md:px-5 xl:max-w-[1280px] 2xl:max-w-full overflow-x-hidden">
+        <div className="mx-auto  flex flex-col lg:flex-row items-stretch justify-center  pt-5 md:pt-6 lg:pt-2 gap-3 sm:px-5 xl:max-w-[1280px] 2xl:max-w-full overflow-x-hidden">
             {/* Left slide */}
             <div className="w-full  lg:w-1/2 h-full">
                 <LeftSlide />
