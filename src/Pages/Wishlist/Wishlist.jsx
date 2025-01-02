c0acd8d8f5515178e05f76831e5dd9b6c728d620
Ridwanul Islam <ridwanul161@gmail.com>
2025-01-02 14:21:13 +0300

    wish page updated

--- src/Pages/Wishlist/Wishlist.jsx
+++ src/Pages/Wishlist/Wishlist.jsx
@@ -148,7 +148,7 @@ const Wishlist = () => {
           )}
 
           {format === "card" && (
-            <div className="my-6 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-x-4 md:gap-y-10 px-20">
+            <div className="my-6 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-x-4 md:gap-y-10 px-20 md:border-2 md:mx-10 py-4 border-gray-300">
               {theUserWishlist.map((wishProduct, index) => (
                 <WishlistCart key={index} wishProduct={wishProduct} />
               ))}
